f717bb0a60f3684e5fbb6038038b1506d76b17b7
Michał <nielipinskim@gmail.com>
2017-07-18 23:27:12 +0200

    Implemented WisielecModellInterface

--- src/Interfaces/WisielecModelInterface.java
+++ src/Interfaces/WisielecModelInterface.java
@@ -1,8 +1,19 @@
 package Interfaces;
 
+import java.util.ArrayList;
+
 /**
  * Created by user_name on 18.07.2017.
  */
 public interface WisielecModelInterface {
+    void initialize();
 
+    String getWord();
+    ArrayList getValidIndexes();
+
+    void enterLetter(char c);
+    void checkForWin();
+
+    void registerObserver(WisielecObserver o);
+    void removeObserver(WisielecObserver o);
 }
