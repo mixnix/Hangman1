4f70ebaa833666e24fe46edbcee6eecac7b4249e
Michał <nielipinskim@gmail.com>
2017-07-21 13:36:49 +0200

    Started imlementing Controller but I have to implement WisielecView first

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,26 @@
 import Interfaces.ControllerInterface;
+import Interfaces.WisielecModelInterface;
 
 import javax.naming.ldap.Control;
 
 /**
  * Created by user_name on 18.07.2017.
  */
-public class Controller {
+public class Controller implements ControllerInterface {
+    WisielecModelInterface model;
+    WisielecView view;
+
+    public Controller(WisielecModelInterface model){
+        this.model = model;
+    }
+    public void startGry(){
+
+    }
+    public void wprowadzenieLiterki(){
+
+    }
+    public void koniecGry(){
+
+    }
 
 }
