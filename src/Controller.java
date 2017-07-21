63d83364f1fd5c8cfadc112218598addca83bb6e
Michał <nielipinskim@gmail.com>
2017-07-21 13:23:57 +0200

    Implemented ControllerInterface

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,10 @@
+import Interfaces.ControllerInterface;
+
+import javax.naming.ldap.Control;
+
 /**
  * Created by user_name on 18.07.2017.
  */
 public class Controller {
+
 }
--- src/Interfaces/ControllerInterface.java
+++ src/Interfaces/ControllerInterface.java
@@ -4,4 +4,7 @@ package Interfaces;
  * Created by user_name on 18.07.2017.
  */
 public interface ControllerInterface {
+    void startGry();
+    void wprowadzenieLiterki();
+    void koniecGry();
 }
