1226fe796158b3fac55ad5a06a1f009456a85192
Michał <nielipinskim@gmail.com>
2017-07-21 20:34:45 +0200

    creating main frame and jPanel with image of wisielec

--- src/WisielecView.java
+++ src/WisielecView.java
@@ -4,6 +4,7 @@ import Interfaces.WisielecObserver;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -19,6 +20,7 @@ public class WisielecView implements ActionListener, WisielecObserver {
 
     //elementy widoku
     JFrame mainViewFrame;
+    //panel z obrazkiem
     JPanel panelHangmanPicture;
     BufferedImage imgHangmanPicture;
     JPanel panelScreenPicture;
@@ -33,11 +35,21 @@ public class WisielecView implements ActionListener, WisielecObserver {
     public void createView(){
         //panel z obrazkiem
         try {
-            imgHangmanPicture = ImageIO.read(new File("wisielec\\"));
+            imgHangmanPicture = ImageIO.read(new File("wisielec\\"+model.getIncorrectGuesses()+".png"));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        JLabel picLabel = new JLabel(new ImageIcon(imgHangmanPicture));
+        panelHangmanPicture = new JPanel(new GridLayout(1,1));
+        panelHangmanPicture.add(picLabel);
 
+        //glowny frame
+        mainViewFrame = new JFrame("View");
+        mainViewFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainViewFrame.setSize(new Dimension(1000,500));
+        mainViewFrame.getContentPane().add(panelHangmanPicture, BorderLayout.CENTER);
+        mainViewFrame.pack();
+        mainViewFrame.setVisible(true);
     }
 
     public void createControls(){
